599cbe7f0dada4a5a718ab4b45e5194fa6b6cd55
Renjie Liu <liurenjie2008@gmail.com>
2020-10-13 21:52:31 +0800

    Add cargo test. (#11)

---
+++ plugin/src/main/kotlin/io/github/liurenjie1024/gradle/rust/CargoTestTask.kt
@@ -0,0 +1,35 @@
+package io.github.liurenjie1024.gradle.rust
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+
+open class CargoTestTask: DefaultTask() {
+    companion object {
+        const val NAME = "cargoTest"
+    }
+
+    @Input
+    val cargoCommand: Property<String> = project.objects.property(String::class.java)
+    @Input
+    var extraCargoBuildArguments: List<String> = emptyList()
+
+    @Suppress("unused")
+    @TaskAction
+    fun clean() {
+        project.exec {
+            it.commandLine = buildCommandLine()
+        }.assertNormalExitValue()
+    }
+
+    fun buildCommandLine(): List<String> {
+        val commandLine = mutableListOf(cargoCommand.get(), "test")
+
+        extraCargoBuildArguments?.let {
+            commandLine.addAll(it)
+        }
+
+        return commandLine
+    }
+}
